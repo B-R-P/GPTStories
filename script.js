e3eeb85defd8c193f6bce231746bccae5179df1a
Bijin Regi Panicker <bijinregipanicker@gmail.com>
2022-12-24 14:51:06 +0530

    Updating script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function addStory(storyData,...other){
 	let [storyName,details] = storyData
 	let heading = document.createElement("h2")
-	heading.style.fontFamily = "Microsoft Sans Serif"
+	heading.style.fontFamily = "Arial"
 	let center = document.createElement("center")
 	center.textContent=storyName
 	heading.append(center)
 	let story = document.createElement("p")
 	story.align = "justify"
-	story.style.fontFamily = "Lexend"
+	story.style.fontFamily = "'Lexend', sans-serif"
 	story.textContent = details["story"]
 	let image = document.createElement("img")
 	image.src="https://raw.githubusercontent.com//B-R-P//GPTStories//main//static//"+details["img"]["src"]
@@ -16,12 +16,10 @@ function addStory(storyData,...other){
 	let storySection = document.createElement("div")
 	storySection.className = "container"
 	storySection.append(heading,story,image)
-	console.log(storySection)
 	return storySection
 }
 async function addStories(url) {
-    const response = await fetch(url,{mode: 'cors'}
-    	)
+    const response = await fetch(url,{mode: 'cors'})
     const json = Object.entries(await response.json())
     document.body.append(...json.map(addStory))
 }
